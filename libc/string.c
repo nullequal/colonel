2f5b8727a83d7852628b03d3b10b535ef82f3dcc
nullequal <nullequal020@proton.me>
2025-02-02 20:36:57 +0200

    Add libc function "memset"

--- libc/string.c
+++ libc/string.c
@@ -1,5 +1,11 @@
 #include "string.h"
 
+void memset(void *s, int c, size_t n) {
+  for (size_t i = 0; i < n; ++i) {
+    ((int*)s)[i] = c;
+  }
+}
+
 size_t strlen(const char *str) {
   const char *s;
   for (s = str; *s; ++s)
--- libc/string.h
+++ libc/string.h
@@ -1,3 +1,4 @@
 #include <stddef.h>
 
+void memset(void *s, int c, size_t n);
 size_t strlen(const char *str);
