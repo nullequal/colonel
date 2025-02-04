406f819169e7878e86bbe477194a0ab74f59ff59
nullequal <nullequal020@proton.me>
2025-02-04 20:43:32 +0200

    Move "strlen" to a string.c file

--- include/printk.h
+++ include/printk.h
@@ -1,12 +1,9 @@
 #ifndef GDT_H
 #define GDT_H
 
-#include <stddef.h>
-#include <stdint.h>
-
+#include "string.h"
 #include "vga.h"
 
-size_t strlen(const char *str);
 void printk(const char *str);
 
 #endif
