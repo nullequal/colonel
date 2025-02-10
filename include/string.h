dfb62a077b13613b4a4017d6fddad0de7d2d7e67
nullequal <nullequal020@proton.me>
2025-02-10 02:32:04 +0200

    Add functions "memcpy" and "memmove"

--- include/string.h
+++ include/string.h
@@ -6,5 +6,7 @@
 
 size_t strlen(const char *str);
 void memset(void *s, int c, size_t n);
+void memcpy(void *d, void *s, size_t n);
+void memmove(void *d, void *s, size_t n);
 
 #endif
--- kernel/string.c
+++ kernel/string.c
@@ -12,3 +12,25 @@ void memset(void *s, int c, size_t n) {
   for (; n != 0; n--)
     *temp++ = c;
 }
+
+void memcpy(void *d, void *s, size_t n) {
+  int *temp_d = (int *)d;
+  const int *temp_s = (int *)s;
+  for (; n != 0; n--) {
+    *temp_d++ = *temp_s++;
+  }
+}
+
+void memmove(void *d, void *s, size_t n) {
+  int *temp_d = (int *)d;
+  int *temp_s = (int *)s;
+  if (d < s) {
+    for (; n != 0; n--) {
+      *temp_d++ = *temp_s++;
+    }
+  } else {
+    for (; n != 0; n--) {
+      temp_d[n - 1] = temp_s[n - 1];
+    }
+  }
+}
