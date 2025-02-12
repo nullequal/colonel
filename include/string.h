2fcdf24dc1f8ecd9cba17fffc82be57210a799fe
nullequal <nullequal020@proton.me>
2025-02-13 00:27:46 +0200

    Improve "mem" functions
    - Change argument symbols
    - Add return values according to specification

--- include/string.h
+++ include/string.h
@@ -5,8 +5,8 @@
 #include <stdint.h>
 
 size_t strlen(const char *str);
-void memset(void *s, int c, size_t n);
-void memcpy(void *d, void *s, size_t n);
-void memmove(void *d, void *s, size_t n);
+void *memset(void *s, int c, size_t n);
+void *memcpy(void *s1, void *s2, size_t n);
+void *memmove(void *s1, void *s2, size_t n);
 
 #endif
--- kernel/string.c
+++ kernel/string.c
@@ -7,30 +7,33 @@ size_t strlen(const char *str) {
   return s - str;
 }
 
-void memset(void *s, int c, size_t n) {
+void *memset(void *s, int c, size_t n) {
   int *temp = (int *)s;
   for (; n != 0; n--)
     *temp++ = c;
+  return s;
 }
 
-void memcpy(void *d, void *s, size_t n) {
-  int *temp_d = (int *)d;
-  const int *temp_s = (int *)s;
+void *memcpy(void *s1, void *s2, size_t n) {
+  int *temp1 = (int *)s1;
+  const int *temp2 = (int *)s2;
   for (; n != 0; n--) {
-    *temp_d++ = *temp_s++;
+    *temp1++ = *temp2++;
   }
+  return s1;
 }
 
-void memmove(void *d, void *s, size_t n) {
-  int *temp_d = (int *)d;
-  int *temp_s = (int *)s;
-  if (d < s) {
+void *memmove(void *s1, void *s2, size_t n) {
+  int *temp1 = (int *)s1;
+  int *temp2 = (int *)s2;
+  if (s1 < s2) {
     for (; n != 0; n--) {
-      *temp_d++ = *temp_s++;
+      *temp1++ = *temp2++;
     }
   } else {
     for (; n != 0; n--) {
-      temp_d[n - 1] = temp_s[n - 1];
+      temp1[n - 1] = temp2[n - 1];
     }
   }
+  return s1;
 }
