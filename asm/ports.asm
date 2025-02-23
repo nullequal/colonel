9ddd0ca04efc3ba40369d8cd42af9fe31c013fec
nullequal <nullequal020@proton.me>
2025-02-23 22:52:16 +0200

    Add functions to access IO ports

---
+++ asm/ports.asm
@@ -0,0 +1,38 @@
+global inb
+inb:
+  mov dx, [esp+4]
+  in al, dx
+  ret
+
+global inw
+inw:
+  mov dx, [esp+4]
+  in ax, dx
+  ret
+
+global inl
+inl:
+  mov dx, [esp+4]
+  in eax, dx
+  ret
+
+global outb
+outb:
+  mov dx, [esp+4]
+  mov al, [esp+8]
+  out dx, al
+  ret
+
+global outw
+outw:
+  mov dx, [esp+4]
+  mov ax, [esp+8]
+  out dx, ax
+  ret
+
+global outl
+outl:
+  mov dx, [esp+4]
+  mov eax, [esp+8]
+  out dx, eax
+  ret
---
+++ include/ports.h
@@ -0,0 +1,14 @@
+#ifndef PORTS_H
+#define PORTS_H
+
+#include <stdint.h>
+
+extern uint8_t inb(uint16_t port);
+extern uint16_t inw(uint16_t port);
+extern uint32_t inl(uint16_t port);
+
+extern void outb(uint16_t port, uint8_t value);
+extern void outw(uint16_t port, uint16_t value);
+extern void outl(uint16_t port, uint32_t value);
+
+#endif
