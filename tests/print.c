72aa8bb836d9ae429d9a3a0eaf6a1918748dfc67
nullequal <nullequal020@proton.me>
2025-02-24 00:53:10 +0200

    Dont begin with 0 in for loop
    Prints an extra color and avoids the black color

--- tests/print.c
+++ tests/print.c
@@ -2,7 +2,7 @@
 #include "vga.h"
 
 void kmain() {
-  for (size_t i = 0; i < 15; ++i) {
+  for (size_t i = 1; i < 16; ++i) {
     scr_set_color(i, VGA_COLOR_BLACK);
     kprintf("%c, %d\n", i + 64, i);
   }
