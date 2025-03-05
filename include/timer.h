fa0e2be064602296d45e565d971839807794100e
nullequal <nullequal020@proton.me>
2025-03-06 01:12:06 +0200

    Add PIT macros for clarity

--- include/timer.h
+++ include/timer.h
@@ -3,6 +3,13 @@
 
 #include "isr.h"
 
+#define PIT_CHANNEL_0_PORT 0x40
+#define PIT_COMMAND 0x43
+#define PIT_CHANNEL_0 0x0
+#define PIT_ACCESS_LOHI 0x30
+#define PIT_MODE_SQUARE_WAVE 0x6
+#define PIT_BINARY 0x0
+
 void pit_handler(registers_t *regs);
 void pit_init();
 
--- kernel/timer.c
+++ kernel/timer.c
@@ -4,7 +4,8 @@
 
 void pit_init() {
   irq_set_handler(0, &pit_handler);
-  outb(0x43, 0x36);
+  outb(0x43,
+       PIT_CHANNEL_0 | PIT_ACCESS_LOHI | PIT_MODE_SQUARE_WAVE | PIT_BINARY);
   outb(0x40, 0xFF);
   outb(0x40, 0xFF);
 }
