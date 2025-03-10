a7582f52ab26293d3f2c6cc71cea070b6a1495eb
nullequal <nullequal020@proton.me>
2025-03-10 21:55:17 +0200

    Dont use hex for the highest frequency divisor

--- kernel/timer.c
+++ kernel/timer.c
@@ -3,10 +3,10 @@
 #include "ports.h"
 
 void pit_init(uint32_t freq_div) {
-  uint16_t quotient = freq_div <= 1 ? 0xFFFF : PIT_CLOCK / freq_div;
   irq_set_handler(0, &pit_handler);
   outb(0x43,
        PIT_CHANNEL_0 | PIT_ACCESS_LOHI | PIT_MODE_SQUARE_WAVE | PIT_BINARY);
+  uint16_t quotient = freq_div <= 1 ? 65535 : PIT_CLOCK / freq_div;
   outb(0x40, quotient & 0xFF);
   outb(0x40, (quotient >> 8) & 0xFF);
 }
